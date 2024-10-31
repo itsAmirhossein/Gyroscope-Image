1081982cf2366c49f9f9c9e86b2585a3b4dfcaba
amirhossein <amirhossein.jahanshahi13@gmail.com>
2024-10-31 07:12:17 +0330

    refactoring  main logic code

--- app/src/main/java/com/jahanshahi/gyroscopeimage/MainActivity.kt
+++ app/src/main/java/com/jahanshahi/gyroscopeimage/MainActivity.kt
@@ -12,9 +12,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -29,8 +41,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import com.jahanshahi.gyroscopeimage.ui.theme.GyroscopeImageTheme
 
@@ -45,9 +59,9 @@ class MainActivity : ComponentActivity() {
             var translationY by remember { mutableFloatStateOf(0f) }
 
             // Define rotation and translation limits and smoothing factors
-            val maxRotation = 5f // Maximum rotation in degrees
+            val maxRotation = 20f // Maximum rotation in degrees
             val maxTranslation = 100f // Maximum translation in pixels (adjust as needed)
-            val smoothingFactor = 0.1f // Adjust this value for smoothing rotation and translation
+            val smoothingFactor = 0.2f // Adjust this value for smoothing rotation and translation
 
             LaunchedEffect(Unit) {
                 val sensorManager =
@@ -60,11 +74,11 @@ class MainActivity : ComponentActivity() {
                             val deltaY = event.values[1] // Rotation around y-axis
 
                             // Smooth out the rotation values
-                            rotationX = (rotationX + deltaX * 15 * smoothingFactor).coerceIn(
+                            rotationX = (rotationX + deltaX * 10 * smoothingFactor).coerceIn(
                                 -maxRotation,
                                 maxRotation
                             )
-                            rotationY = (rotationY + deltaY * 15 * smoothingFactor).coerceIn(
+                            rotationY = (rotationY + deltaY * 10 * smoothingFactor).coerceIn(
                                 -maxRotation,
                                 maxRotation
                             )
@@ -91,33 +105,79 @@ class MainActivity : ComponentActivity() {
             }
 
             GyroscopeImageTheme {
-                Box(
-                    modifier = Modifier.fillMaxSize().background(color = Color(0xFF46080d)),
-                    contentAlignment = Alignment.Center
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.img_bg),
-                        modifier = Modifier.fillMaxSize()
-                            .graphicsLayer(
-                            rotationX = rotationY,
-                            rotationY = rotationX
-                        ),
-                        contentScale = ContentScale.Crop,
-                        contentDescription = "CR7 Image",
-                    )
-                    Image(
-                        painter = painterResource(id = R.drawable.img_fg),
-                        modifier = Modifier.fillMaxSize().padding(horizontal = 48.dp)
-                            .zIndex(2f)
-                            .graphicsLayer(
-                                rotationX = rotationX,
-                                rotationY = rotationY
+                    Box(
+                        modifier = Modifier.weight(8f).fillMaxSize().background(Color(0xFFDA020E)),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Card (
+                            shape = RoundedCornerShape(16.dp),
+                            elevation = CardDefaults.cardElevation(
+                                defaultElevation = 8.dp
                             ),
-                        contentScale = ContentScale.Crop,
-                        contentDescription = "CR7 Image",
-                    )
+                            modifier = Modifier
+                                .size(200.dp)
+                                .graphicsLayer(
+                                    rotationX = rotationX,
+                                    rotationY = rotationY
+                                ),
+                            colors = CardDefaults.cardColors(
+                                containerColor = Color.White
+                            )
+                        ){
+                            Image(
+                                painter = painterResource(id = R.drawable.manutd),
+                                modifier = Modifier
+                                    .size(200.dp).padding(20.dp),
+                                contentScale = ContentScale.Crop,
+                                contentDescription = "CR7 Image",
+                            )
+                        }
+                    }
+                    Column(
+                        modifier = Modifier
+                            .weight(2f).fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp),
+                    ) {
+                        Text(
+                            text = "rotationX: $rotationX",
+                            modifier = Modifier.fillMaxWidth(),
+                            style = TextStyle(
+                                color = Color.Black,
+                                fontSize = 16.sp
+                            )
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "rotationY: $rotationY",
+                            modifier = Modifier.fillMaxWidth(),
+                            style = TextStyle(
+                                color = Color.Black,
+                                fontSize = 16.sp
+                            )
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "translationX: $translationX",
+                            modifier = Modifier.fillMaxWidth(),
+                            style = TextStyle(
+                                color = Color.Black,
+                                fontSize = 16.sp
+                            )
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "translationY: $translationY",
+                            modifier = Modifier.fillMaxWidth(),
+                            style = TextStyle(
+                                color = Color.Black,
+                                fontSize = 16.sp
+                            )
+                        )
+                    }
                 }
-
             }
         }
     }
